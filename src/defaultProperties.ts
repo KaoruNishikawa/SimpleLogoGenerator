3ded0a9816d90d411b60c6a14303f01a71e0fd09
Kaoru Nishikawa <kq.nishikawa@gmail.com>
2023-07-02 05:19:15 +0900

    #67 Use URL parameters

--- src/defaultProperties.ts
+++ src/defaultProperties.ts
@@ -1,14 +1,21 @@
 import { LogoProperty } from "./types";
+import { URLParameter } from "./features/url";
 
+
+const urlParams = new URLParameter();
 
 const defaultProperty: LogoProperty = {
-    text: "logo",
-    size: { x: 80, y: 80 },
-    font: { familyIdx: 0, variantIdx: 0, size: 20 },
-    textColor: { color1: "#FF5599", color2: "#55FFFF", angle: 45, width: 35 },
-    textOffset: { x: 0, y: 0 },
-    bgColor: { color1: "#003344", color2: "#005533", angle: 45, width: 50 },
-    bgOpacity: 100,
+    text: urlParams.get("text", "logo"),
+    size: urlParams.getJSON("size", { x: 80, y: 80 }),
+    font: urlParams.getJSON("font", { familyIdx: 0, variantIdx: 0, size: 20 }),
+    textColor: urlParams.getJSON(
+        "textColor", { color1: "#FF5599", color2: "#55FFFF", angle: 45, width: 35 }
+    ),
+    textOffset: urlParams.getJSON("textOffset", { x: 0, y: 0 }),
+    bgColor: urlParams.getJSON(
+        "bgColor", { color1: "#003344", color2: "#005533", angle: 45, width: 50 }
+    ),
+    bgOpacity: urlParams.getInt("bgOpacity", 100),
 }
 
 
