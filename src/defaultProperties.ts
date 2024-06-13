cffbbb0716fa9ed9d3234c8fe8c7d988e729bfa8
Kaoru Nishikawa <kq.nishikawa@gmail.com>
2024-06-14 01:05:23 +0900

    Change font object properties

--- src/defaultProperties.ts
+++ src/defaultProperties.ts
@@ -1,13 +1,19 @@
 import { LogoProperty } from "./types";
 import { URLParameter } from "./features/url";
+import { fontList } from "./features/fonts";
 
+
+const initialFont = fontList[Math.floor(Math.random() * fontList.length)];
+const initialVariant = initialFont.variants.includes("regular")
+    ? "regular"
+    : initialFont.variants[0];
 
 const urlParams = new URLParameter();
 
 const defaultProperty: LogoProperty = {
     text: urlParams.get("text", "logo"),
     size: urlParams.getJSON("size", { x: 80, y: 80 }),
-    font: urlParams.getJSON("font", { familyIdx: 0, variantIdx: 0, size: 20 }),
+    font: urlParams.getJSON("font", { size: 20, family: initialFont.family, variant: initialVariant}),
     textColor: urlParams.getJSON(
         "textColor", { color1: "#FF5599", color2: "#55FFFF", angle: 45, width: 35 }
     ),
